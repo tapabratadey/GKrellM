4d494a5e0badd4fa61809f0c9459cdcea30034bd
Brendan <brendan_1391@yahoo.com>
2018-07-08 14:38:36 -0700

    Updated main

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,11 @@
 /*   By: maghayev <maghayev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 02:53:03 by maghayev          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2018/07/07 15:05:45 by tadey            ###   ########.fr       */
+=======
+/*   Updated: 2018/07/07 15:09:25 by bpierce          ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +24,18 @@
 #include "CPUModule.class.hpp"
 #include "RAMModule.class.hpp"
 #include "Network.class.hpp"
+#include "Minilibx.class.hpp"
+#include "Graph.class.hpp"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "mlx.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 
 int main() {
@@ -67,6 +83,31 @@ int main() {
 
 	std::map<std::string, std::string> mapnet = net_mod.getData();
 	std::cout << "Network throughput: " << mapnet["network"] << '\n';	
+
+	Minilibx	m;
+	Graph		g(m.getMlx());
+
+	for (int i = 0 ; i < 1000 ; i++) {
+		g.addToHistory(rand() % 1000);
+	}
+
+	g.backgroundFill(2132127);
+	g.setX(200);
+	g.setY(200);
+	g.fillDataAll();
+	g.drawToScreen(m.getWin());
+
+	m.screenDraw(gen_mod);
+	m.screenDraw(os_mod);
+	m.screenDraw(time_mod);
+	m.screenDraw(cpu_mod);
+
+	while (1)
+	{
+		m.screenDraw();
+		m.screenRefresh();
+	}
+	// mlx_loop(m.getMlx());
 
 	return 0;
 }
