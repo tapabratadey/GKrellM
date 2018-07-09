6033b1ea7aed1ad4a503ce7d958de5b53771ede8
Maksud Aghayev <maghayev@e1z4r14p10.42.us.org>
2018-07-08 17:53:19 -0700

    Moved ncludes in header

--- sources/BaseBase.class.cpp
+++ sources/BaseBase.class.cpp
@@ -6,18 +6,11 @@
 /*   By: maghayev <maghayev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 16:34:45 by maghayev          #+#    #+#             */
-/*   Updated: 2018/07/08 17:38:36 by maghayev         ###   ########.fr       */
+/*   Updated: 2018/07/08 17:52:39 by maghayev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BaseBase.class.hpp"
-#include "CPUModule.class.hpp"
-#include "DateTimeModule.class.hpp"
-#include "GenericModule.class.hpp"
-#include "OSModule.class.hpp"
-#include "RAMModule.class.hpp"
-#include "Network.class.hpp"
-
 
 BaseBase::BaseBase() {
 	initData();
